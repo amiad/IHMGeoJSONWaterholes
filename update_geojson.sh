84892fd2521f7c9913830286a9f97a899ed5ca17
amiad <amiad@bareli.localdomain>
2020-12-09 00:49:16 +0200

    create update_geojson.sh

---
+++ update_geojson.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+url="https://docs.google.com/spreadsheets/d/1BbT762qaYilLnwGD7sJDo8sfs_Y5ExjvixS_Tl3rSas/gviz/tq?tqx=out:csv&sheet=IHM"
+csv='floods.csv'
+wget  $url -O $csv
+./node_modules/csv2geojson/csv2geojson --lat Latitude --lon Longitude floods.csv > source.geojson
+git add .
+git commit -m "update source.geojson $(date +%Y-%m-%d-%R)"
+git push
