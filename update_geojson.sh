1eaf778edf26ea4a3250342f00f54be3debcdd4d
amiad <amiad@hatul.info>
2021-11-20 21:51:11 +0200

    merging ihm sheet to main

--- update_geojson.sh
+++ update_geojson.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-url="https://docs.google.com/spreadsheets/d/1BbT762qaYilLnwGD7sJDo8sfs_Y5ExjvixS_Tl3rSas/gviz/tq?tqx=out:csv&sheet=IHM"
+url="https://docs.google.com/spreadsheets/d/1BbT762qaYilLnwGD7sJDo8sfs_Y5ExjvixS_Tl3rSas/gviz/tq?tqx=out:csv&sheet=ראשי"
 csv='floods.csv'
 
 pushd $(dirname "$0") > /dev/null
 git checkout main
 git pull
 wget  $url -O $csv
+tail -n +4 $csv > $csv
+
 ./node_modules/csv2geojson/csv2geojson --lat Latitude --lon Longitude $csv > source.geojson
 git add .
 git commit source.geojson $csv -m "update source.geojson $(date +%Y-%m-%d-%R)"
