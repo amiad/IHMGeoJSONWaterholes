cab1eb02b2e613309abb39c745e0046bb2e21ee7
amiad <amiad@hatul.info>
2021-11-22 22:32:38 +0200

    check merging

--- update_geojson.sh
+++ update_geojson.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-url="https://docs.google.com/spreadsheets/d/1BbT762qaYilLnwGD7sJDo8sfs_Y5ExjvixS_Tl3rSas/gviz/tq?tqx=out:csv&sheet=ראשי"
+url="https://docs.google.com/spreadsheets/d/1BbT762qaYilLnwGD7sJDo8sfs_Y5ExjvixS_Tl3rSas/export?format=csv&sheet=ראשי&range=A4:M100&headers=0"
 csv='floods.csv'
 
 pushd $(dirname "$0") > /dev/null
 #git checkout main
 git pull
 wget  $url -O $csv
-tail -n +4 $csv > $csv
 
 ./node_modules/csv2geojson/csv2geojson --lat Latitude --lon Longitude $csv > source.geojson
 git add .
 git commit source.geojson $csv -m "update source.geojson $(date +%Y-%m-%d-%R)"
 git push
 popd > /dev/null
+
+# https://stackoverflow.com/questions/33713084/download-link-for-google-spreadsheets-csv-export-with-multiple-sheets
