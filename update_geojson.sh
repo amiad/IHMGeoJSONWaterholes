3cf8eb20decffdeba5ec2684f96700f0a7b2415a
amiad <amiad@hatul.info>
2021-11-23 12:03:55 +0200

    test

--- update_geojson.sh
+++ update_geojson.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 url="https://docs.google.com/spreadsheets/d/1BbT762qaYilLnwGD7sJDo8sfs_Y5ExjvixS_Tl3rSas/export?format=csv&sheet=ראשי&range=A4:M100&headers=0"
 csv='floods.csv'
+head="stream,flood_date,flood_source,name,status,date,source,source2,notes,OSMId,Latitude,Longitude,Id"
 
 pushd $(dirname "$0") > /dev/null
 #git checkout main
 git pull
-wget  $url -O $csv
+wget $url -O $csv
+sed -i "1s/.*/$head/" $csv
 
 ./node_modules/csv2geojson/csv2geojson --lat Latitude --lon Longitude $csv > source.geojson
 git add .
